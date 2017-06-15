1a0ca67be78054798c8cf4f0af5f6ec48e106e5a
Mahmoud Ben Hassine <mahmoud.benhassine@icloud.com>
2017-06-15 23:07:12 +0200

    extract database type from configuration file

--- easy-jobs-dist/src/assembly/bin/run.sh
+++ easy-jobs-dist/src/assembly/bin/run.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 set -e
 
+if [ "$#" -ne 1 ]; then
+    echo "Please provide the path to Easy Jobs configuration file"
+    exit 1;
+fi
+
 CONFIG_FILE=$1
 WORKING_DIR=`pwd`
 ROOT_DIR=`dirname ${WORKING_DIR}`
-DB_TYPE=H2 # todo extract db type from config file
+DB_TYPE=$(cat ${CONFIG_FILE} | grep 'database.type' | cut -d : -f 2 | tr '"' ' ' | tr ',' ' ' | sed 's/^ *//;s/ *$//')
 
 java -cp "${ROOT_DIR}/jobs/*:${ROOT_DIR}/lib/*:${ROOT_DIR}/drivers/${DB_TYPE}/*" \
  -Djava.util.logging.config.file=${ROOT_DIR}/conf/logging.properties \
- org.jeasy.jobs.JobServer
+ -Deasy.jobs.configuration.path=${CONFIG_FILE} \
+ org.jeasy.jobs.JobServer &
