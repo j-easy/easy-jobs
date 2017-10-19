75c2367971770e7cb8b6ba2227b6cd97bd69bf26
Mahmoud Ben Hassine <mahmoud.benhassine@icloud.com>
2017-10-19 22:14:17 +0200

    add method to get page title as model attribute

--- easy-jobs-admin/src/main/java/org/jeasy/jobs/admin/web/AbstractController.java
+++ easy-jobs-admin/src/main/java/org/jeasy/jobs/admin/web/AbstractController.java
@@ -2,11 +2,14 @@ package org.jeasy.jobs.admin.web;
 
 import org.springframework.web.bind.annotation.ModelAttribute;
 
-public class AbstractController {
+public abstract class AbstractController {
 
     @ModelAttribute("version")
     public String version() {
         // TODO get current version
         return "0.3-SNAPSHOT";
     }
+
+    @ModelAttribute
+    public abstract String title();
 }
