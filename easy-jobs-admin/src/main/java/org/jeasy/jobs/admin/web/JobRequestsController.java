5915b8ca1dc2a1de47da1a2e7e445c3bb8ce6392
Mahmoud Ben Hassine <mahmoud.benhassine@icloud.com>
2017-10-24 23:44:08 +0200

    add form to create a new job execution request

--- easy-jobs-admin/src/main/java/org/jeasy/jobs/admin/web/JobRequestsController.java
+++ easy-jobs-admin/src/main/java/org/jeasy/jobs/admin/web/JobRequestsController.java
@@ -1,23 +1,48 @@
 package org.jeasy.jobs.admin.web;
 
+import org.jeasy.jobs.job.JobRepository;
+import org.jeasy.jobs.request.JobRequest;
 import org.jeasy.jobs.request.JobRequestRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static java.lang.Integer.parseInt;
 
 @Controller
 public class JobRequestsController extends AbstractController {
 
     @Autowired
+    private JobRepository jobRepository;
+
+    @Autowired
     private JobRequestRepository jobRequestRepository;
 
     @RequestMapping("/requests")
-    public ModelAndView jobs() {
+    public ModelAndView requests() {
         ModelAndView modelAndView = new ModelAndView("requests");
         modelAndView.addObject("requests", jobRequestRepository.findAllJobRequests());
         return modelAndView;
+    }
+
+    @RequestMapping(path = "/requests/new", method = RequestMethod.GET)
+    public ModelAndView newJobExecutionRequestForm() {
+        ModelAndView modelAndView = new ModelAndView("new-request");
+        modelAndView.addObject("jobs", jobRepository.findAll());
+        return modelAndView;
+    }
+
+    @RequestMapping(path = "/requests/new", method = RequestMethod.POST)
+    public ModelAndView createNewJobExecutionRequest(HttpServletRequest request) {
+        String jobId = request.getParameter("jobId");
+        String jobParameters = request.getParameter("jobParameters");
+        jobRequestRepository.save(new JobRequest(parseInt(jobId), jobParameters));
+        return requests();
     }
 
     @ModelAttribute("title")
